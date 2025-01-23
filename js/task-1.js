b850c8a423f53604f402f74c6df4fe064c353cc2
Brain-Lady <brain.lady404@gmail.com>
2025-01-23 21:47:36 +0200

    task1-3

---
+++ js/task-1.js
@@ -0,0 +1,10 @@
+const categories = document.querySelectorAll('#categories .item');
+
+console.log('Number of categories:', categories.length);
+
+categories.forEach(category => {
+  const heading = category.querySelector('h2').textContent;
+  const elems = category.querySelectorAll('ul > li');
+  console.log(`Category: ${heading}`);
+  console.log(`Elements: ${elems.length}`);
+});
---
+++ js/task-3.js
@@ -0,0 +1,14 @@
+const input = document.getElementById("name-input");
+const output = document.getElementById("name-output");
+
+input.addEventListener("input", function () {
+  let name = input.value.trim();
+  output.textContent = name === "" ? "Anonymous" : name;
+});
+
+
+
+Напиши скрипт, який під час набору тексту в інпуті input#name-input (подія input) підставляє його поточне значення в span#name-output як ім’я для привітання. Обов’язково очищай значення в інпуті по краях від пробілів . Якщо інпут порожній або містить лише пробіли, то замість імені у спан має підставлятися рядок "Anonymous".
+
+
+
